ace73d4392bb100160c1ffa7319cf1053dff24cc
ponomarev-iv <design-mr@mail.ru>
2020-05-25 12:43:58 +0500

    fix maasonry grid in antikorryption Main page

--- pages/company/antikorrupcionnaya-deyatelnost/index.vue
+++ pages/company/antikorrupcionnaya-deyatelnost/index.vue
@@ -12,24 +12,23 @@
         )
         h1.section-title.section-title_mrg-xl {{ title }}
         .masonry.masonry__wrap.masonry__wrap_mrg-sm.masonry_category.mb_xxl
-          .masonry__col.masonry__col_half
+          .masonry__col.masonry__col_pad-mid
+            nuxt-link.catalog-item.catalog-item_long.catalog-item_l-blue(to="/company/antikorrupcionnaya-deyatelnost/antikorrupcionnaya-politika")
+              h2.catalog-item__name Антикоррупционная <br>политика
+              span.catalog-item__btn Подробнее
+          .masonry__col.masonry__col_pad-mid
             .masonry__row.masonry__row_mrg-mid
-              nuxt-link.catalog-item.catalog-item_ll-blue.catalog-item_violet.catalog-item_height-mid(to="/company/antikorrupcionnaya-deyatelnost/antikorrupcionnaya-politika")
-                h2.catalog-item__name.catalog-item__name_xl Антикоррупционная <br>политика
+              nuxt-link.catalog-item.catalog-item_llll-blue(to="/company/antikorrupcionnaya-deyatelnost/normativnye-pravovye-i-inye-akty")
+                h2.catalog-item__name Нормативные правовые <br>и&nbsp;иные акты в&nbsp;сфере <br>противодействия <br>коррупции
                 span.catalog-item__btn Подробнее
             .masonry__row
-              nuxt-link.catalog-item.catalog-item_llll-blue.catalog-item_blue.catalog-item_height-mid(to="/company/antikorrupcionnaya-deyatelnost/formy-dokumentov")
-                h2.catalog-item__name.catalog-item__name_xl Формы документов, <br>связанных <br>с&nbsp;противодействием коррупции, <br>для&nbsp;заполнения
+              nuxt-link.catalog-item.catalog-item_ll-blue(to="/company/antikorrupcionnaya-deyatelnost/obratnaya-svyaz-dlya-soobshchenij-o-faktah-korrupcii")
+                h2.catalog-item__name Обратная связь для&nbsp;сообщений <br>о&nbsp;фактах коррупции
                 span.catalog-item__btn Подробнее
-          .masonry__col.masonry__col_half
-            .masonry__row.masonry__row_mrg-mid
-              nuxt-link.catalog-item.catalog-item_l-blue.catalog-item_green.catalog-item_height-mid(to="/company/antikorrupcionnaya-deyatelnost/normativnye-pravovye-i-inye-akty")
-                h2.catalog-item__name.catalog-item__name_xl Нормативные правовые <br>и&nbsp;иные акты в&nbsp;сфере <br>противодействия коррупции
-                span.catalog-item__btn Подробнее
-            .masonry__row
-              nuxt-link.catalog-item.catalog-item_ll-blue.catalog-item_violet.catalog-item_height-mid(to="/company/antikorrupcionnaya-deyatelnost/obratnaya-svyaz-dlya-soobshchenij-o-faktah-korrupcii")
-                h2.catalog-item__name.catalog-item__name_xl Обратная связь для&nbsp;сообщений <br>о&nbsp;фактах коррупции
-                span.catalog-item__btn Подробнее
+          .masonry__col.masonry__col_pad-mid
+            nuxt-link.catalog-item.catalog-item_long.catalog-item_l-blue(to="/company/antikorrupcionnaya-deyatelnost/formy-dokumentov")
+              h2.catalog-item__name Формы документов, <br>связанных <br>с&nbsp;противодействием коррупции, <br>для&nbsp;заполнения
+              span.catalog-item__btn Подробнее
     TheFooter
 </template>
 
