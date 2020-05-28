8a947cf6267e741d0d6724f072efb2008993c1e0
ponomarev-iv <ponomarev-iv@galagodigital.ru>
2020-05-28 10:57:39 +0500

    fix to main footer and fotter to all

--- components/TheMainFooter.vue
+++ components/TheMainFooter.vue
@@ -2,9 +2,9 @@
   footer.main-footer.main-footer_fixed(ref="foot")
     .c-wrap
       .main-footer__nav
-        router-link.main-footer__link(to="in-dev") Оставить заявку
-        router-link.main-footer__link(to="in-dev") Заказать звонок
-        router-link.main-footer__link(to="in-dev") Презентация
+        a.main-footer__link(href="mailto:info@globalenergoholding.ru") Оставить заявку
+        a.main-footer__link(href="/doc/referens-list-ao-globalehnergoholding.pdf") Референс-лист
+        a.main-footer__link(href="/doc/kp-ao-globalehnergoholding.pdf") Презентация
       .main-footer__rigth-col.soc-link
         a.soc-link__item.soc-link__item_vk(href="#" target="_blank")
         a.soc-link__item.soc-link__item_fb(href="#" target="_blank")
